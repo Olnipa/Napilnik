095568c9d3ff870af5ee5c82850023416ded8832
Pakhimskii ON <oleghack@gmail.com>
2023-07-09 17:39:51 +0700

    Напильник. Инкапсуляция. Разбор инкапсуляции кода.

--- Napilnik/Program.cs
+++ Napilnik/Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace Napilnik
 {
-    internal class Program
+    class Weapon
     {
-        static void Main(string[] args)
+        public int Damage { get; private set; }
+        public int Bullets { get; private set; }
+
+        public void Fire(Player player)
         {
-            Console.WriteLine("Hello, World!");
+            player.TakeDamage(Damage);
+            Bullets -= 1;
+        }
+    }
+
+    class Player
+    {
+        public int Health { get; private set; }
+
+        public void TakeDamage(int damage)
+        {
+            if (damage > 0)
+                Health -= damage;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon Weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.Fire(player);
         }
     }
 }
