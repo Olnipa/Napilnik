ab981d164989b8354d632c5d3288d5c82e627f70
Pakhimskii ON <oleghack@gmail.com>
2023-07-16 20:02:47 +0700

    Loger Initial commit

---
+++ Loging/Loger.cs
@@ -0,0 +1,62 @@
+﻿namespace Loging
+{
+    interface ILogger
+    {
+        void WriteError(string message);
+    }
+
+    class BaseLogWriter : ILogger
+    {
+        public virtual void WriteError(string message) { }
+    } 
+
+    class ConsoleLogWriter : ILogger
+    {
+        private ILogger _logger;
+
+        public ConsoleLogWriter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public virtual void WriteError(string message)
+        {
+            Console.WriteLine(message);
+            _logger.WriteError(message);
+        }
+    }
+
+    class FileLogWriter : ILogger
+    {
+        private ILogger _logger;
+
+        public FileLogWriter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteError(string message)
+        {
+            File.WriteAllText("log.txt", message);
+            _logger.WriteError(message);
+        }
+    }
+
+    class SecureLogWriter : ILogger
+    {
+        private ILogger _logger;
+
+        public SecureLogWriter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                _logger.WriteError(message);
+            }
+        }
+    }
+}
---
+++ Loging/Program.cs
@@ -0,0 +1,43 @@
+﻿using Loging;
+
+namespace Napilnik
+{
+    internal class Program
+    {
+        private const string FileLog1 = "LogMessage1";
+        private const string FileLog2 = "LogMessage2";
+        private const string FileLog3 = "LogMessage3";
+        private const string FileLog4 = "LogMessage4";
+        private const string FileLog5 = "LogMessage5";
+
+        static void Main(string[] args)
+        {
+            Pathfinder pathfinder1 = new Pathfinder(new FileLogWriter(new BaseLogWriter()));
+            Pathfinder pathfinder2 = new Pathfinder(new ConsoleLogWriter(new BaseLogWriter()));
+            Pathfinder pathfinder3 = new Pathfinder(new SecureLogWriter(new FileLogWriter(new BaseLogWriter())));
+            Pathfinder pathfinder4 = new Pathfinder(new SecureLogWriter(new ConsoleLogWriter(new BaseLogWriter())));
+            Pathfinder pathfinder5 = new Pathfinder(new ConsoleLogWriter(new SecureLogWriter(new FileLogWriter(new BaseLogWriter()))));
+
+            pathfinder1.Find(FileLog1);
+            pathfinder2.Find(FileLog2);
+            pathfinder3.Find(FileLog3);
+            pathfinder4.Find(FileLog4);
+            pathfinder5.Find(FileLog5);
+        }
+    }
+
+    class Pathfinder
+    {
+        private ILogger Logger;
+
+        public Pathfinder(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Find(string message)
+        {
+            Logger.WriteError(message);
+        }
+    }
+}
