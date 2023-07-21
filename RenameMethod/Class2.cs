560ed575a18be57d2c91e54cbe1315386761b4dd
Pakhimskii ON <oleghack@gmail.com>
2023-07-21 19:30:02 +0700

    Переименовал методы

---
+++ RenameMethod/Class2.cs
@@ -0,0 +1,20 @@
+﻿namespace CleanCode
+{
+    internal class Class2
+    {
+        public static void CreateNewObject()
+        {
+            //Создание объекта на карте
+        }
+
+        public static void GenerateChance()
+        {
+            _chance = Random.Range(0, 100);
+        }
+
+        public static int GetSalary(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
+        }
+    }
+}
