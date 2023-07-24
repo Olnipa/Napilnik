c80eed7ef377b1a65b3b52d2cc38757bd4f58216
Pakhimskii ON <oleghack@gmail.com>
2023-07-24 20:11:23 +0700

    Избавился от аргумента-флага, разделив его на 2 метода

---
+++ Task30/Program.cs
@@ -0,0 +1,14 @@
+﻿class Task30
+{
+    public void EnableAnimation()
+    {
+        _effects.StartEnableAnimation();
+        _enable = true;
+    }
+
+    public void DoSomethingWithPool()
+    {
+        _pool.Free(this);
+        _enable = false;
+    }
+}
